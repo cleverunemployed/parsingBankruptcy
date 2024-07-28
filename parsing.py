16d723472ff13989656f0f2389c319780639a71f
kassslll <kasslesson@gmail.com>
2024-07-28 11:26:43 +0400

    create function which collection data from page

--- parsing.py
+++ parsing.py
@@ -3,18 +3,37 @@ from bs4 import BeautifulSoup
 import time
 
 
+def parsing_data(driver: Chrome, url: str) -> list[list[str]]:
+    driver.get(url)
+    time.sleep(5)
+    result: list[list[str]] = list()
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    table = soup.find("table", attrs={"class": "bank"}).find("tbody")
+    rows: list = table.find_all("tr")[:-2]
+
+    for tr in rows:
+        data: list[str] = []
+        if tr.find_all("th") != []:
+            for th in tr.find_all("th"):
+                data.append(th.text.replace("\n", "").replace("\t", ""))
+        else:
+            for td in tr.find_all("td"):
+                data.append(td.text.replace("\n", "").replace("\t", ""))
+        result.append(data)
+
+    return result
+
+
 def data_collection(url: str) -> list[list[str]]:
 
     data: list = []
     driver: Chrome = Chrome()
-    driver.get(url=url)
 
-    time.sleep(5)
-
-    
-
-
-    return list
+    data = parsing_data(driver=driver, url=url)
+    [print(x) for x in data]
+    return data
 
 
 def main() -> None:
